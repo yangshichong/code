35e8352e1ab21309ec6a34a06de73267be9b64b6
yangshichong <1004234087@qq.com>
2017-07-20 18:06:46 +0800

    计算器注释

--- 计算器/main.cpp
+++ 计算器/main.cpp
@@ -1,7 +1,13 @@
-/*如何识别数字还是运算符
+/**
+计算器思路：两个栈，数字栈和运算符栈（都是数值栈，用不同数字代表+-/*），依次遍历字符串，数字则直接入数字栈；运算符比栈顶运算符优先级大则入栈；
+否则，从栈顶弹栈，再取数字栈顶的两个栈，进行运算，结果再压入数字栈。为了判断运算是否结束，故在栈的开头结尾
+设置优先级最低的标记运算符。当运算符栈仅剩两个标记运算符时，运算结束；数字栈的唯一元素即为结果。
+
 运算符的优先级用二维数组的0或1判断
-数字和运算符之间用空格间隔是为了可以运算两位及其以上的数字
-*/
+
+关键在于函数void getop(bool &retop,int &retin,int &index)，从字符串中取数字或者运算符，先判断
+是否到了字符串尾；再分数字或者运算符处理。bool retop标记操作符/数字，retin返回数值或操作符，index作为字符串下标
+**/
 #include <stdio.h>
 #include<stack>
 #include <stdlib.h>
@@ -11,12 +17,21 @@ char str[200];
 stack<int> stackop;
 stack<double> stackin;
 int priority[][5]={1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,0,0,1,1,1,0,0};
+/**引用就是给变量起别名，相当于使用了指针，对引用的操作就是对原变量的操作，解决了参数传递不改变原值的缺点**/
 void getop(bool &retop,int &retin,int &index){
     int temp;
-    if(str[index]!=NULL)///没到字符尾部
+    if(str[index]!=0)///没到字符尾部
     {
+        if(str[index]=='x'){
+            retop=false;
+            printf("请输入x的值：");
+            scanf("%d",&retin);
+            getchar();///必须把缓冲区的换行符接收，否则影响gets()
+            index++;
+            return;
+        }
             retin=0;
-            while(str[index]>='0'&&str[index]<='9'){//是数字，记录数值
+            while(str[index]>='0'&&str[index]<='9'){///是数字，记录数值
                 retop=false;
                 temp=str[index]-'0';
                 retin=retin*10+temp;
@@ -42,7 +57,7 @@ void getop(bool &retop,int &retin,int &index){
             }
 
     }
-            if(str[index]==NULL)///遍历到字符串尾部
+            if(str[index]==0)///遍历到字符串尾部
             {
                 retop=true;
                 retin=0;
@@ -67,12 +82,12 @@ int main()
     stackop.push(0);///放入标记运算符
 
     while(true){
-    getop(reto,retn,i);//从字符串取元素
-    if(reto){                   //是操作符
-        if(stackop.empty()||priority[retn][stackop.top()]==1)//当前运算符优先级大于栈顶运算符
+    getop(reto,retn,i);///从字符串取元素
+    if(reto){                   ///是操作符
+        if(stackop.empty()||priority[retn][stackop.top()]==1)///当前运算符优先级大于栈顶运算符
             stackop.push(retn);
         else{
-        while(priority[retn][stackop.top()]==0){                       //当前运算符优先级小于栈顶运算符
+        while(priority[retn][stackop.top()]==0){                       ///当前运算符优先级小于栈顶运算符
             a=stackin.top();
             stackin.pop();
             b=stackin.top();
@@ -87,9 +102,9 @@ int main()
                 temp=b*a;
             if(c==4)
                 temp=b/a;
-            stackin.push(temp);
+            stackin.push(temp);///运算结果入数字栈
         }
-        stackop.push(retn);
+        stackop.push(retn);///当前运算符优先级大于栈顶运算符优先级，则当前运算符入栈。
         }
     }
     else//是数字
@@ -98,7 +113,7 @@ int main()
 
     }
 
-    if(stackop.size()==2&&stackin.size()==1&&stackop.top()==0)
+    if(stackop.size()==2&&stackop.top()==0)
         break;
 
 
